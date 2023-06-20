2a02a0f2d2e31e4dcb45e78a1ab4f6ffb15879c8
mizar <mizarzulfaz@gmail.com>
2023-06-20 22:41:46 +0700

    Title changed

---
+++ 2_Discord_Bot_Message_Logger.py
@@ -0,0 +1,17 @@
+import discord
+from auth.privateToken import myToken
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(myToken())
